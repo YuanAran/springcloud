8b88ed5f1664907fb3b9a6adfe5a59c8cd1ceca2
YuanAran <19185402557@163.com>
2025-09-10 09:33:30 +0800

    first

---
+++ database-provider/src/test/java/com/databaseprovider/DatabaseProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.databaseprovider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseProviderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
