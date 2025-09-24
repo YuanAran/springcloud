38b0b830164ac2210048cb03e8a30e0611c2cf48
YuanAran <19185402557@163.com>
2025-09-24 16:08:48 +0800

    Delete database-provider directory

--- database-provider/src/main/java/com/databaseprovider/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.databaseprovider.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.databaseprovider.pojo.SysUser;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface UserMapper extends BaseMapper<SysUser> {
-}
