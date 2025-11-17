023be245e67ee0030f851704dcf401210f98f55a
YuanAran <19185402557@163.com>
2025-11-17 10:11:30 +0800

    25

--- database-provider/src/main/java/Test.java
+++
@@ -1,9 +0,0 @@
-import com.databaseprovider.controller.DbController;
-import com.databaseprovider.controller.DeptController;
-import com.databaseprovider.service.RoleService;
-
-public class Test {
-    public static void main(String[] args) {
-        RoleService roleService = new RoleService();
-    }
-}
